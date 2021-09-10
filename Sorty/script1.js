e1ab4c1ee6a642867510bbe4c647cc8613f0db88
Pai026 <abhirampai1999@gmail.com>
2021-09-10 22:10:53 +0530

    added event listeners for login and signup buttons

--- Sorty/script1.js
+++ Sorty/script1.js
@@ -12,6 +12,30 @@ width: 100%;
 align-items:basline;
 `;
 
+let loginModal = document.createElement("div");
+loginModal.style.display = "none";
+loginModal.style.cssText += `
+position: absolute;
+top:50%;
+left:50%;
+transform:translate(-50%,-50%);
+max-width: 100%;
+padding: 5rem;
+background-color: white;
+`;
+
+let signUpModal = document.createElement("div");
+signUpModal.style.display = "none";
+signUpModal.style.cssText += `
+position: absolute;
+top:50%;
+left:50%;
+transform:translate(-50%,-50%);
+max-width: 100%;
+padding: 5rem;
+background-color: white;
+`;
+
 const CreateElementAndAppend = (
   value,
   style = null,
@@ -22,7 +46,7 @@ const CreateElementAndAppend = (
   let tagName = document.createElement(tag);
   tagName.innerHTML = value;
   style !== null && (tagName.style.cssText += style);
-  onClick !== null && tagName.addEventListner("click", onClick);
+  onClick !== null && tagName.addEventListener("click", onClick);
   parent.appendChild(tagName);
 };
 
@@ -62,17 +86,28 @@ CreateElementAndAppend(
   leftLinks
 );
 
+const onClickLogin = () => {
+  loginModal.style.display = "block";
+  main.style.opacity = 0.4;
+};
+
 CreateElementAndAppend(
   "Login",
-  `font-size:1.5rem;margin-top:0.2rem;`,
-  null,
+  `font-size:1.5rem;margin-top:0.2rem;cursor: pointer;`,
+  onClickLogin,
   "a",
   rightLinks
 );
+
+const onClickSubmit = () => {
+  signUpModal.style.display = "block";
+  main.style.opacity = 0.4;
+};
+
 CreateElementAndAppend(
   "SignUp",
-  `border-radius:2.5rem;padding:1.2rem;margin-top: -1.2rem;border-color:transparent;background-color:#8EE5D1;font-size:1.5rem`,
-  null,
+  `border-radius:2.5rem;padding:1.2rem;cursor: pointer;margin-top: -1.2rem;border-color:transparent;background-color:#8EE5D1;font-size:1.5rem`,
+  onClickSubmit,
   "button",
   rightLinks
 );
@@ -113,14 +148,14 @@ CreateElementAndAppend(
 );
 CreateElementAndAppend(
   "Get Started",
-  `border-radius:4.5rem;padding:1.2rem;margin-bottom: 1.2rem;border-color:transparent;background-color:#8EE5D1;font-size:1.5rem`,
+  `border-radius:4.5rem;padding:1.2rem;cursor: pointer;margin-bottom: 1.2rem;border-color:transparent;background-color:#8EE5D1;font-size:1.5rem`,
   null,
   `button`,
   leftSide
 );
 
 let image = document.createElement("img");
-image.src = "../TB3-assessments/css-assessment/illustration.png";
+image.src = "../../TB3-assessments/css-assessment/illustration.png";
 
 image.style.cssText += `
 width:50vw;
@@ -135,3 +170,152 @@ section.appendChild(leftSide);
 section.appendChild(rightSide);
 main.appendChild(navbar);
 main.appendChild(section);
+
+document.body.appendChild(loginModal);
+document.body.appendChild(signUpModal);
+
+let modalHeading = document.createElement("h1");
+modalHeading.innerHTML = "Login";
+modalHeading.style.textAlign = "center";
+
+let modalHeading1 = document.createElement("h1");
+modalHeading1.innerHTML = "Sign Up";
+modalHeading1.style.textAlign = "center";
+
+let form = document.createElement("form");
+form.style.cssText += `
+display: flex;
+flex-direction: column;
+row-gap: 1.2rem;
+padding: 0 10rem;
+`;
+
+let form1 = document.createElement("form");
+form1.style.cssText += `
+display: flex;
+flex-direction: column;
+row-gap: 1.2rem;
+padding: 0 10rem;
+`;
+
+let input = document.createElement("input");
+input.type = "text";
+input.placeholder = "oliver@example.com";
+input.style.cssText += `
+border-radius: 0.5rem;
+height: 2rem;
+text-align: center;
+`;
+
+let closeBtn = document.createElement("span");
+closeBtn.innerHTML = "&times";
+closeBtn.style.cssText += `
+  color: black;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+let closeBtn1 = document.createElement("span");
+closeBtn1.innerHTML = "&times";
+closeBtn1.style.cssText += `
+  color: black;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+let input1 = document.createElement("input");
+input1.type = "text";
+input1.placeholder = "oliver@example.com";
+input1.style.cssText += `
+border-radius: 0.5rem;
+height: 2rem;
+text-align: center;
+`;
+
+let password = document.createElement("input");
+password.type = "password";
+password.placeholder = "*******";
+password.style.cssText += `
+border-radius: 0.5rem;
+height: 2rem;
+text-align: center;
+`;
+
+let password1 = document.createElement("input");
+password1.type = "password";
+password1.placeholder = "*******";
+password1.style.cssText += `
+border-radius: 0.5rem;
+height: 2rem;
+text-align: center;
+`;
+
+let confirmPassword = document.createElement("input");
+confirmPassword.type = "password";
+confirmPassword.placeholder = "*******";
+confirmPassword.style.cssText += `
+border-radius: 0.5rem;
+height: 2rem;
+text-align: center;
+`;
+
+let submitBtn = document.createElement("button");
+submitBtn.innerHTML = "Submit";
+submitBtn.style.cssText += `
+border-radius:1.5rem;
+padding:0.85rem;
+margin-bottom: 1.2rem;
+border-color:transparent;
+background-color:#8EE5D1;
+font-size:0.875rem;
+cursor: pointer;
+`;
+
+let submitBtn1 = document.createElement("button");
+submitBtn1.innerHTML = "Submit";
+submitBtn1.style.cssText += `
+border-radius:1.5rem;
+padding:0.85rem;
+margin-bottom: 1.2rem;
+border-color:transparent;
+background-color:#8EE5D1;
+font-size:0.875rem;
+cursor: pointer;
+`;
+
+const onClickSubmitInLoginModal = (e) => {
+  e.preventDefault();
+  loginModal.style.display = "none";
+  main.style.opacity = 1;
+};
+
+const onClickSubmitInSignUpModal = (e) => {
+  e.preventDefault();
+  signUpModal.style.display = "none";
+  loginModal.style.display = "none";
+  main.style.opacity = 1;
+};
+
+submitBtn.addEventListener("click", onClickSubmitInLoginModal);
+closeBtn.addEventListener("click", onClickSubmitInLoginModal);
+submitBtn1.addEventListener("click", onClickSubmitInSignUpModal);
+closeBtn1.addEventListener("click", onClickSubmitInSignUpModal);
+
+loginModal.appendChild(closeBtn);
+loginModal.appendChild(modalHeading);
+loginModal.appendChild(form);
+form.appendChild(input);
+form.appendChild(password);
+form.appendChild(submitBtn);
+
+signUpModal.appendChild(closeBtn1);
+signUpModal.appendChild(modalHeading1);
+signUpModal.append(form1);
+form1.append(input1);
+form1.append(password1);
+form1.append(confirmPassword);
+form1.append(submitBtn1);
